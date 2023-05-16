075057fbeb9b90eed0fdbd427d7b7d7ab9e6c729
unknown <rishab2245@gmail.com>
2023-05-16 19:52:49 +0530

    assignment

--- matrix.cpp
+++ matrix.cpp
@@ -1,21 +1,25 @@
 //making row and colume of the matrix zero if the element is zero
 #include<iostream>
+#include<vector>
 using namespace std;
 int main()
-{
-    int a[3][3];
-    for (int i = 0; i < 3; i++)
+{   int m,n;
+    cout<<"Enter the size of the array m,n:";
+    cin>>m>>n;
+    int a[m][n];
+    for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < n; j++)
         {
             cin>>a[i][j];
         }
+        cout<<"\n";
         
     }
-    int b[3][3];
-     for (int i = 0; i < 3; i++)
+    vector<vector<int>>b(m,vector<int>(n,-1));
+     for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < n; j++)
         {
             if (a[i][j]==0)
             {
@@ -25,24 +29,27 @@ int main()
         }
         
     }
-     for (int i = 0; i < 3; i++)
+     for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < n; j++)
         {
             if(b[i][j]==0){
-                for (int k = 0; k < 3; k++)
+                for (int k = 0; k < m ; k++)
+                {
+                    a[k][j]=0;
+                }
+                for (int k = 0; k < n ; k++)
                 {
                     a[i][k]=0;
-                    a[k][j]=0;
                 }
                 
             }
         }
         
     }
-       for (int i = 0; i < 3; i++)
+       for (int i = 0; i < m; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < n; j++)
         {
             cout<<a[i][j]<<" ";
         }
